037673535698dd2e4484b011f4af51e6e2c9ec55
Justin-PC <justinkowalsky@live.com>
2017-02-15 01:30:24 -0500

    Loop works. Needs finalization

--- YelpReviewScraper/YelpReviewScraper/Form1.cs
+++ YelpReviewScraper/YelpReviewScraper/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using System.Text.RegularExpressions;
 
 namespace YelpReviewScraper
 {
@@ -26,32 +27,38 @@ namespace YelpReviewScraper
 
         private void startSearch_Click(object sender, EventArgs e)
         {
-            
-            IWebElement myProductSpot = driverGC.FindElement(By.Id("find_desc"));
+            //searches product and company and clicks search
+            IWebElement myProductSpot = driverGC.FindElement(By.CssSelector("#find_desc"));
             myProductSpot.SendKeys(myProduct.Text);
-            IWebElement myLocationSpot = driverGC.FindElement(By.Id("dropperText_Mast"));
+            IWebElement myLocationSpot = driverGC.FindElement(By.CssSelector("#dropperText_Mast"));
             myLocationSpot.Clear();
             myLocationSpot.SendKeys(myLocation.Text);
-            driverGC.FindElement(By.Id("header-search-submit")).Click();
+            driverGC.FindElement(By.CssSelector("#header-search-submit")).Click();
             driverGC.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
-            //var dataKey =  driverGC.FindElement(By.ClassName("search-result")).GetAttribute("data-key");
-            //IList<IWebElement> dataKeys = driverGC.FindElement(By.ClassName("search-result")).GetAttribute("data-key" + i).ToList();
-            //MessageBox.Show(dataKeys.ToString());
-            var dataKey = driverGC.FindElement(By.ClassName("search-result")).GetAttribute("data-key");
-            int newDataKey = Int32.Parse(dataKey);
-            MessageBox.Show(dataKey.ToString());
-            for (int i=1; i<10; i++)
-            {
-                var starRating = driverGC.FindElement(By.ClassName("i-stars"));
-                MessageBox.Show(starRating.GetAttribute("title"));
-                driverGC.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
-                var bizLocation = driverGC.FindElement(By.ClassName("secondary-attributes"));
-                MessageBox.Show(bizLocation.Text);
-                newDataKey = Int32.Parse(dataKey) + i;
-                MessageBox.Show(newDataKey.ToString());
 
-            }     
+            List<IWebElement> shittyBiz = new List<IWebElement>();
+            var myEles = driverGC.FindElements(By.CssSelector("div.search-result"));
             
+            foreach (IWebElement business in myEles)
+            {
+                var starRating = business.FindElement(By.CssSelector("div.biz-rating > div.i-stars")).GetAttribute("title");
+                starRating = Regex.Replace(starRating, @"[A-Za-z\s]", string.Empty);
+                float stars = float.Parse(starRating);
+                MessageBox.Show(stars.ToString());
+                if (stars <= 3)
+                {
+                    MessageBox.Show("Shitty");
+                    
+                }
+                else
+                {
+                    driverGC.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5));
+                    var bizLocation = business.FindElement(By.CssSelector(".secondary-attributes"));
+                    MessageBox.Show(bizLocation.Text);
+                    
+                }
+                
+            }     
         }
     }
 }
